00ed1c5dc112be975fba21b4c40cdf4053ab6970
wenping-xie <144593369+wenping-xie@users.noreply.github.com>
2025-12-25 17:14:20 +0800

    model.py

--- model.py
+++ model.py
@@ -83,7 +83,7 @@ def AlexNet(nb_classes,input_shape):
     input_ten = Input(shape=input_shape)
     #1
     x = tf.keras.layers.Conv1D(filters=96,kernel_size=(11,11),strides=(4,4),activation='relu')(input_ten)
-    x = BatchNormalization()(x)##利用BN代替LRN
+    x = BatchNormalization()(x)
     x = MaxPooling1D(pool_size=(3,3),strides=2)(x)
     #2
     x = tf.keras.layers.Conv1D(filters=256,kernel_size=(5,5),strides=(1,1),activation='relu',padding='same')(x)
@@ -107,7 +107,6 @@ def AlexNet(nb_classes,input_shape):
     return model
 
 
-# 导入工具包
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -115,31 +114,19 @@ from tensorflow.keras import layers
 def AlexNet1D():
     # 模型构建
     model = keras.models.Sequential([
-        # 卷积：卷积核数量96，尺寸11*11，步长4，激活函数relu
         layers.Conv1D(filters=96, kernel_size=11, strides=4, activation='relu'),
-        # 最大池化：尺寸3*3，步长2
         layers.MaxPool1D(pool_size=3, strides=2),
-        # 卷积：卷积核数量256，尺寸5*5，激活函数relu，same卷积
         layers.Conv1D(filters=256, kernel_size=5, padding='same', activation='relu'),
-        # 最大池化：尺寸3*3，步长3
         layers.MaxPool1D(pool_size=3, strides=2),
-        # 卷积：卷积核数量384，尺寸3，激活函数relu，same卷积
         layers.Conv1D(filters=384, kernel_size=3, padding='same', activation='relu'),
-        # 卷积：卷积核数量384，尺寸3，激活函数relu，same卷积
         layers.Conv1D(filters=384, kernel_size=3, padding='same', activation='relu'),
-        # 卷积：卷积核数量256，尺寸3，激活函数relu，same卷积
         layers.Conv1D(filters=256, kernel_size=3, padding='same', activation='relu'),
-        # 最大池化：尺寸3*3，步长2
         layers.MaxPool1D(pool_size=3, strides=2),
-        # 展平特征图
         layers.Flatten(),
-        # 全连接：4096神经元，relu
         layers.Dense(4096, activation='relu'),
-        # 随机失活
         layers.Dropout(0.5),
         layers.Dense(4096, activation='relu'),
         layers.Dropout(0.5),
-        # 输出层：多分类用softmax，二分类用sigmoid
         layers.Dense(2, activation='softmax')],
         name='AlexNet')
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=1.0, clipvalue=0.5),
@@ -147,33 +134,21 @@ def AlexNet1D():
     
     return model
 
-# 模拟输入
+
 net = keras.models.Sequential([
-    # 卷积：卷积核数量96，尺寸11*11，步长4，激活函数relu
     layers.Conv1D(filters=96, kernel_size=11, strides=4, activation='relu'),
-    # 最大池化：尺寸3*3，步长2
     layers.MaxPool1D(pool_size=3, strides=2),
-    # 卷积：卷积核数量256，尺寸5*5，激活函数relu，same卷积
     layers.Conv1D(filters=256, kernel_size=5, padding='same', activation='relu'),
-    # 最大池化：尺寸3*3，步长3
     layers.MaxPool1D(pool_size=3, strides=2),
-    # 卷积：卷积核数量384，尺寸3，激活函数relu，same卷积
     layers.Conv1D(filters=384, kernel_size=3, padding='same', activation='relu'),
-    # 卷积：卷积核数量384，尺寸3，激活函数relu，same卷积
     layers.Conv1D(filters=384, kernel_size=3, padding='same', activation='relu'),
-    # 卷积：卷积核数量256，尺寸3，激活函数relu，same卷积
     layers.Conv1D(filters=256, kernel_size=3, padding='same', activation='relu'),
-    # 最大池化：尺寸3*3，步长2
     layers.MaxPool1D(pool_size=3, strides=2),
-    # 展平特征图
     layers.Flatten(),
-    # 全连接：4096神经元，relu
     layers.Dense(4096, activation='relu'),
-    # 随机失活
     layers.Dropout(0.5),
     layers.Dense(4096, activation='relu'),
     layers.Dropout(0.5),
-    # 输出层：多分类用softmax，二分类用sigmoid
     layers.Dense(2, activation='softmax')],
     name='AlexNet')
 x = tf.random.uniform((1,327,14))
@@ -184,3 +159,4 @@ net.summary()
 # y = net(x)
 
 # net.summary()
+
